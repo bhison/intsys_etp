b014717aea6d9646da3611f78a5ec4e124c50861
bhison <catbat04@gmail.com>
2015-04-04 23:51:32 +0100

    Done most of the class, just trying to get PrintWriter to deal with its exceptions

--- IntSys_ETP/src/build1/Main.java
+++ IntSys_ETP/src/build1/Main.java
@@ -1,10 +1,12 @@
 package build1;
 
+import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.LinkedList;
 
-public class Main {
-
+public class Main 
+{
+	
 	private int gameWidth;
 	private int gameHeight;
 	
@@ -19,19 +21,15 @@ public class Main {
 		else createSolutionFile(puzzleString, solutionPath);
 	}
 	
-	//ITERATIVE DEEPENING
 	private LinkedList<char[]> iterativeDeepening(char[] start, char[] dest)
-	{
-		for (int depth = 1; true; depth++) // doubtful termination
-		{
-			LinkedList<char[]> route = depthFirst(start, dest, depth);
+	{	for (int depth = 1; true; depth++) // doubtful termination
+		{	LinkedList<char[]> route = depthFirst(start, dest, depth);
 			if (route != null) return route; // fast exit
 		}
 	}
 	
-	//DEPTH FIRST LIMITED SEARCH
-    private LinkedList<char[]> depthFirst(char[] start, char[] dest, int depth) {
-    	if (depth == 0) return null;
+    private LinkedList<char[]> depthFirst(char[] start, char[] dest, int depth) 
+    {  	if (depth == 0) return null;
 		else if (start.equals(dest))
 		{	LinkedList<char[]> route = new LinkedList<char[]>(); 
 			route.add(dest); // construct singleton route
@@ -48,17 +46,38 @@ public class Main {
 			}
 			return null; 
 		}
-	}
+	}  
     
     private LinkedList<char[]> getAdjacencies(char[] config)
-    {	
-    	return null;
+    {	int gapIndex = 999;
+    	for(int i = 0; i < config.length; i++)
+			if(config[i] == '_') 
+			{	gapIndex = i;
+				break; //Might need to check if this is actually doing anything
+			}
+    	LinkedList<String> moveList = getValidMoveList(gapIndex);
+    	LinkedList<char[]> adjacencyList = new LinkedList<char[]>();
+    	for(String direction : moveList)
+    	{	
+    		int tileIndex = gapIndex;
+    		if(direction == "up") tileIndex += gameWidth;
+    		else if(direction == "down") tileIndex -= gameWidth;
+    		else if(direction == "left") tileIndex -= 1;
+    		else if(direction == "right") tileIndex += 1;
+    		
+    		char[] newConfig = config.clone();
+    		char tileValue = newConfig[tileIndex];
+    		newConfig[tileIndex] = '_';
+    		newConfig[gapIndex] = tileValue;	
+    		adjacencyList.add(newConfig);
+    	}
+    	return adjacencyList;
     }
 	
 	/**
-	 * Takes a gap index
+	 * Takes a gap index and returns list of valid slides based on game dimensions
 	 * @param gapIndex
-	 * @return
+	 * @return List of valid slide directions
 	 */
 	private LinkedList<String> getValidMoveList(int gapIndex)
 	{	LinkedList<String> returnArray = new LinkedList<String>(Arrays.asList("up", "right", "down", "left"));
@@ -69,9 +88,24 @@ public class Main {
 		if(column == gameWidth - 1) returnArray.remove("left");
 		return returnArray;
 	}
-	
+
 	private void createSolutionFile(String puzzleString, LinkedList<char[]> solutionPath)
-	{
+	{	PrintWriter writer = new PrintWriter(puzzleString, "UTF-8");
+		for(int i = 0; i < gameHeight; i++)
+		{	StringBuilder line = new StringBuilder();
+			for(char[] config : solutionPath)
+			{	StringBuilder configLine = new StringBuilder();
+				for(int j = 0; j < gameWidth; j++)
+					configLine.append(config[j + (i * gameWidth)]);
+				configLine.append(" ");
+				line.append(configLine);
+			}
+			writer.println(line);
+		}
+		writer.close();
+		
+		
+		
 		//Create a file in same place as jar with filename of puzzleString
 		//RULES:
 		// > Should only write to first four lines
@@ -87,8 +121,7 @@ public class Main {
 	}
 	
 	public static void main(String[] args)
-	{
-		int gameWidth = 3; //Change this to alter default		
+	{	int gameWidth = 3; //Change this to alter default		
 		if(args.length > 1) gameWidth = Integer.parseInt(args[1]);
 		if((args[0].length() - 5) / 2 % gameWidth == 0) new Main(args[0], gameWidth);
 		else System.out.println("ERROR: Invalid game width/puzzle size combo");
