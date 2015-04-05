dda41cd1647816d85ae43dfffd99dd19223e078f
bhison <catbat04@gmail.com>
2015-04-05 04:47:10 +0100

    Theoretically 'workig' version

---
+++ IntSys_ETP/src/build1/BatchSolve.java
@@ -0,0 +1,119 @@
+package build1;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class BatchSolve {
+	public BatchSolve(String[] args)
+	{	
+		List<String> puzzles;
+		String puzzleWidth = null;
+		if(args.length > 1) puzzleWidth = args[1];
+		try {
+			puzzles = Files.readAllLines(Paths.get(args[0]), StandardCharsets.US_ASCII);
+			System.out.println("loadDat: Data loaded internally");
+			for(String puzzle : puzzles)
+				System.out.println(puzzle);
+			for(String puzzle : puzzles)
+			{	String[] a;
+				if(args.length > 1)
+					a = new String[]{ puzzle, args[1] };
+				else
+					a = new String[]{ puzzle };
+				new Main(a);			
+			}
+		}
+		catch (IOException e) {
+			System.out.println("IOException: " + e.toString());
+			return;
+		}
+		
+	}
+}
+
+
+//package build1;
+////This may end up being useful: new ArrayList<Element>(Arrays.asList(array))
+//import java.io.IOException;
+//import java.nio.charset.StandardCharsets;
+//import java.nio.file.Files;
+//import java.nio.file.Paths;
+//import java.util.List;
+//
+//import org.jfree.data.xy.XYSeries;
+//import org.jfree.data.xy.XYSeriesCollection;
+//
+//public class DatLoader {
+//
+//private Double[][] dataList;
+//private Double[] xValues;
+//private Double[] yValues;
+//private XYSeries xySeries;
+//private XYSeriesCollection xySeriesCollection;
+//private int dataSize;
+//
+//public DatLoader() {
+//	List<String> datFile;
+//	try {
+//		datFile = Files.readAllLines(Paths.get("datfile.dat"), StandardCharsets.US_ASCII);
+//		System.out.println("loadDat: Data loaded internally");
+//	}
+//	catch (IOException e) {
+//		System.out.println("IOException: " + e.toString());
+//		return;
+//	}
+//	
+//	dataSize = datFile.size();
+//	
+//	dataList = new Double[dataSize][2];
+//	xValues = new Double[dataSize];
+//	yValues = new Double[dataSize];
+//	int i = 0;
+//	for (String line : datFile) {
+//	    String[] array = line.split("\\s+");
+//	    xValues[i] = Double.parseDouble(array[0]);
+//	    yValues[i] = Double.parseDouble(array[1]);
+//	    dataList[i] = new Double[]{xValues[i], yValues[i]};
+//	    i++;
+//	}
+//	
+//	xySeries = createSeries(dataList);
+//	xySeriesCollection = new XYSeriesCollection(xySeries);
+//}
+//
+//private XYSeries createSeries(Double[][] data){
+//	XYSeries returnSeries = new XYSeries("TargetCurve");
+//	for(Double[] entry : data){
+//		returnSeries.add(entry[0], entry[1]);
+//	}
+//	return returnSeries;
+//}
+//
+//public Double[][] getDataList(){
+//	return dataList;
+//}
+//
+//public XYSeries getXYSeries(){
+//	return xySeries;
+//}
+//
+//public XYSeriesCollection getXYSeriesCollection(){
+//	return xySeriesCollection;
+//}
+//
+//public Double[] getXValues(){
+//	return xValues;
+//}
+//
+//public Double[] getYValues(){
+//	return yValues;
+//}
+//
+//public int getDataSize(){
+//	return dataSize;
+//}
+//
+//}
--- IntSys_ETP/src/build1/Main.java
+++ IntSys_ETP/src/build1/Main.java
@@ -1,5 +1,6 @@
 package build1;
 
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.LinkedList;
@@ -10,43 +11,93 @@ public class Main
 	private int gameWidth;
 	private int gameHeight;
 	
-	public Main(String puzzleString, int gameWidth)
-	{	this.gameWidth = gameWidth;
+	private static final boolean OUTPUT_TO_CONSOLE = false; //Set this to false to output as files
+	
+	//public Main(String puzzleString, int gameWidth)
+	public Main(String[] args)
+	{	String puzzleString = args[0];
+		int gameWidth = 3; //Change this to alter default
+		if(args.length > 1) gameWidth = Integer.parseInt(args[1]);
+		this.gameWidth = gameWidth;
+		System.out.println("gameWidth set to: " + gameWidth);
 		int puzzleSize = (puzzleString.length() -5 ) /2;
-		gameHeight =  puzzleSize /gameWidth;
+		System.out.println("puzzleSize set to: " + puzzleSize);
+		gameHeight =  puzzleSize / gameWidth;
+		System.out.println("gameHeight set to: " + gameHeight);
 		char[] start = puzzleString.substring(0, puzzleSize).toCharArray();
 		char[] target = puzzleString.substring(puzzleSize + 1, puzzleSize * 2 + 1).toCharArray();
+		System.out.println("Puzzle string is: " + puzzleString);
+		System.out.println("start set to: " + new String(start));
+		System.out.println("target set to: " + new String(target));
 		LinkedList<char[]> solutionPath = iterativeDeepening(start,target);
 		if(solutionPath == null) System.out.println("ERROR: Something went wrong! No solution found!");
-		else createSolutionFile(puzzleString, solutionPath);
+		else 
+		{	try	{ createSolutionFile(puzzleString, solutionPath, OUTPUT_TO_CONSOLE); }
+			catch(IOException e) { System.out.println(e); }
+		}
 	}
 	
 	private LinkedList<char[]> iterativeDeepening(char[] start, char[] dest)
 	{	for (int depth = 1; true; depth++) // doubtful termination
-		{	LinkedList<char[]> route = depthFirst(start, dest, depth);
+		{	System.out.println("******************************************** DEPTH = " + depth + " *******");
+			LinkedList<char[]> route = new LinkedList<char[]>(Arrays.asList(start));
+			route = depthFirst(route, dest, depth);
 			if (route != null) return route; // fast exit
 		}
 	}
 	
-    private LinkedList<char[]> depthFirst(char[] start, char[] dest, int depth) 
+//    private LinkedList<char[]> depthFirst(char[] start, char[] dest, int depth) 
+//    {  	if (depth == 0) return null;
+//		else if (Arrays.equals(start, dest))
+//		{	LinkedList<char[]> route = new LinkedList<char[]>(); 
+//			route.add(dest); // construct singleton route
+//			return route;	
+//		}
+//		else 
+//		{	LinkedList<char[]> nextConfigs = getAdjacencies(start); 
+//			for (char[] next : nextConfigs) // search top-down
+//			{	LinkedList<char[]> route = depthFirst(next, dest, depth - 1);
+//				if (route != null)
+//				{	route.addFirst(start);
+//					return route;	
+//				}
+//			}
+//			return null; 
+//		}
+//    }
+    
+    
+	private LinkedList<char[]> depthFirst(LinkedList<char[]> route, char[] dest, int depth)
     {  	if (depth == 0) return null;
-		else if (start.equals(dest))
-		{	LinkedList<char[]> route = new LinkedList<char[]>(); 
-			route.add(dest); // construct singleton route
-			return route;	
-		}
-		else 
-		{	LinkedList<char[]> nextConfigs = getAdjacencies(start); 
-			for (char[] next : nextConfigs) // search top-down
-			{	LinkedList<char[]> route = depthFirst(next, dest, depth - 1);
-				if (route != null)
-				{	route.addFirst(start);
-					return route;	
+		char[] last = route.getLast();
+		//System.out.println("Last: " + new String(last));
+		//System.out.println("Dest: " + new String(dest));
+		if (new String(last).equals(new String(dest))) 
+		{ 	System.out.println("Last = Dest"); 
+			return route; 
+			}
+		else
+		{	LinkedList<char[]> nextConfigs = getAdjacencies(route.getLast());
+			for (char[] next:nextConfigs)
+			{
+				if (!route.contains(next))
+				{
+					LinkedList<char[]> nextRoute = (LinkedList<char[]>) route.clone();
+					nextRoute.add(next);
+					LinkedList<char[]> wholeRoute =
+						depthFirst(nextRoute, dest, depth - 1); 
+					if (wholeRoute != null) return wholeRoute;
 				}
 			}
-			return null; 
 		}
+		return null;
 	}  
+    
+    
+    
+    
+    
+    
     
     private LinkedList<char[]> getAdjacencies(char[] config)
     {	int gapIndex = 999;
@@ -58,17 +109,19 @@ public class Main
     	LinkedList<String> moveList = getValidMoveList(gapIndex);
     	LinkedList<char[]> adjacencyList = new LinkedList<char[]>();
     	for(String direction : moveList)
-    	{	
-    		int tileIndex = gapIndex;
+    	{	int tileIndex = gapIndex;
     		if(direction == "up") tileIndex += gameWidth;
     		else if(direction == "down") tileIndex -= gameWidth;
-    		else if(direction == "left") tileIndex -= 1;
-    		else if(direction == "right") tileIndex += 1;
+    		else if(direction == "left") tileIndex += 1;
+    		else if(direction == "right") tileIndex -= 1;
     		
     		char[] newConfig = config.clone();
+    		//System.out.println("getAdjacencies.newCofig set to: " + new String(newConfig));
     		char tileValue = newConfig[tileIndex];
+    		
     		newConfig[tileIndex] = '_';
-    		newConfig[gapIndex] = tileValue;	
+    		newConfig[gapIndex] = tileValue;
+    		//System.out.println("newConfig of: " + new String(newConfig));
     		adjacencyList.add(newConfig);
     	}
     	return adjacencyList;
@@ -86,11 +139,19 @@ public class Main
 		int column = gapIndex % gameWidth;
 		if(column == 0) returnArray.remove("right");
 		if(column == gameWidth - 1) returnArray.remove("left");
+		
+		//Delete this after debug
+		//System.out.println("Valid moves for gapIndex " + gapIndex + ":");
+//		for(String direction : returnArray)
+//			System.out.println(" >" + direction);
+//			
 		return returnArray;
 	}
 
-	private void createSolutionFile(String puzzleString, LinkedList<char[]> solutionPath)
-	{	PrintWriter writer = new PrintWriter(puzzleString, "UTF-8");
+	private void createSolutionFile(String puzzleString, LinkedList<char[]> solutionPath, 
+			boolean consoleMode) throws IOException
+	{	PrintWriter writer = null;
+		if(!consoleMode) writer = new PrintWriter(puzzleString, "UTF-8");
 		for(int i = 0; i < gameHeight; i++)
 		{	StringBuilder line = new StringBuilder();
 			for(char[] config : solutionPath)
@@ -100,31 +161,15 @@ public class Main
 				configLine.append(" ");
 				line.append(configLine);
 			}
-			writer.println(line);
+			if(consoleMode) System.out.println(line);
+			else writer.println(line);
 		}
-		writer.close();
-		
-		
-		
-		//Create a file in same place as jar with filename of puzzleString
-		//RULES:
-		// > Should only write to first four lines
-		// > Should use spaces as delimiters
-		// > Should be in style:
-		//
-		// 		aaa aaa aaa aaa aaa aaa aaa
-		// 		aaa aaa aaa aaa aaa aaa aaa
-		// 		aaa aaa aaa aaa aaa aaa aaa
-		// 		aaa aaa aaa aaa aaa aaa aaa
-		// 
-		// > Start should be on left, solution on right with intermediate configurations in between
+		if(!consoleMode) writer.close();
 	}
 	
 	public static void main(String[] args)
-	{	int gameWidth = 3; //Change this to alter default		
-		if(args.length > 1) gameWidth = Integer.parseInt(args[1]);
-		if((args[0].length() - 5) / 2 % gameWidth == 0) new Main(args[0], gameWidth);
-		else System.out.println("ERROR: Invalid game width/puzzle size combo");
+	{	if(args[0].substring(0,5).toLowerCase().equals("batch"))
+			new BatchSolve(args);
+		else new Main(args);
 	}	
-
 }
